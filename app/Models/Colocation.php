510e4510bab5170541e1f351804e78e3f32fb49b
ajartijaouad <ajartijaouad14@gmail.com>
2026-02-24 13:10:33 +0000

    feat: configure Colocation model with fillable and relationships

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Colocation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'invitation_token', 'status'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)
+                    ->withPivot('role', 'joined_at', 'left_at')
+                    ->withTimestamps();
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function settlements()
+    {
+        return $this->hasMany(Settlement::class);
+    }
 }
