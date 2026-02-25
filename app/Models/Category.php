3348aa372a286cd30d9b131c8b38554aeb751ea6
ajartijaouad <ajartijaouad14@gmail.com>
2026-02-25 09:31:05 +0000

    feat: setup Category model fields and relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name', 'colocation_id'];
+
+    public function colocation()
+    {
+        return $this->belongsTo(colocation::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
