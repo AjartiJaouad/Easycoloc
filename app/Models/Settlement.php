7ee0fa87a76874567980ece193d5d33c4e0cf68e
ajartijaouad <ajartijaouad14@gmail.com>
2026-02-25 09:40:41 +0000

    feat: implement Settlement model relationships and fillable fields

--- app/Models/Settlement.php
+++ app/Models/Settlement.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Settlement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['colocation_id', 'debtor_id', 'creditor_id', 'amount', 'is_paid'];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function debtor()
+    {
+        return $this->belongsTo(User::class, 'debtor_id');
+    }
+
+    public function creditor()
+    {
+        return $this->belongsTo(User::class, 'creditor_id');
+    }
 }
