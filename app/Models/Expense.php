fe210b125135247a0b3b8da7e0a3cab76918589e
ajartijaouad <ajartijaouad14@gmail.com>
2026-02-25 09:36:29 +0000

    feat: define Expense model fillable properties and relations

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'amount', 'date', 'colocation_id', 'user_id', 'category_id'];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
